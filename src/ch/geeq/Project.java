0a7049e1bb51ef578298ad5edeaad0bbbf4fcbf0
Weby <weby@we-bb.com>
2017-02-24 11:03:37 +0100

    Instanciated new datapoints.

--- src/ch/geeq/Project.java
+++ src/ch/geeq/Project.java
@@ -1,5 +1,7 @@
 package ch.geeq;
 
+import ch.geeq.datapoint.DataPoint;
+
 /**
  * @author weby@we-bb.com [Nicolas Glassey]
  * @version 1.0.0
@@ -7,6 +9,10 @@ package ch.geeq;
  */
 public class Project {
     public static void main(String[] args) {
-        System.out.println("Hello, world !");
+        DataPoint boolIn = new DataPoint("boolIn", false);
+        DataPoint boolOut = new DataPoint("boolOut", true);
+        
+        DataPoint floatIn = new DataPoint("floatIn", false);
+        DataPoint floatOut = new DataPoint("floatOut", true);
     }
 }
