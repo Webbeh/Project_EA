59098c5527a7bc27b73cc94efcd6b92ab0c198ca
Weby <weby@we-bb.com>
2017-02-24 10:23:56 +0100

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author weby@we-bb.com [Nicolas Glassey]
+ * @version 1.0.0
+ * @since 2/24/17
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
