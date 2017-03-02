5447a9e72067f527086c2e6de385764237639314
Weby <weby@we-bb.com>
2017-03-02 08:44:46 +0100

    Added Coil and InputRegister classes.

--- src/ch/geeq/Coil.java
+++ src/ch/geeq/Coil.java
@@ -1,10 +1,17 @@
 package ch.geeq;
 
+import ch.geeq.datapoint.BinaryDataPoint;
+
 /**
  * @author weby@we-bb.com [Nicolas Glassey]
  * @version 1.0.0
  * @since 3/2/17
  */
 public class Coil {
-
+    private int coilAddress, rtuAddress;
+    private BinaryDataPoint itsBinaryDataPoint;
+    
+    public void write() {
+    
+    }
 }
---
+++ src/ch/geeq/InputRegister.java
@@ -0,0 +1,17 @@
+package ch.geeq;
+
+import ch.geeq.datapoint.FloatDataPoint;
+
+/**
+ * @author weby@we-bb.com [Nicolas Glassey]
+ * @version 1.0.0
+ * @since 3/2/17
+ */
+public class InputRegister {
+    private int inputRegisterAddress, rtuAddress;
+    private FloatDataPoint itsFloatDataPoint;
+    
+    public void read() {
+    
+    }
+}
