6156a5165053658cdc3a6951db3088a135ce981e
sieri <sieri@windowslive.com>
2017-03-03 14:57:49 +0100

    Connected database connector to the database

--- src/main/java/ch/geeq/connectors/DataBaseConnector.java
+++ src/main/java/ch/geeq/connectors/DataBaseConnector.java
@@ -4,6 +4,11 @@ import ch.geeq.datapoint.BinaryDataPoint;
 import ch.geeq.datapoint.DataPoint;
 import ch.geeq.datapoint.FloatDataPoint;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * @author weby@we-bb.com [Nicolas Glassey]
  * @version 1.0.0
@@ -12,10 +17,36 @@ import ch.geeq.datapoint.FloatDataPoint;
 public class DataBaseConnector
 {
 
+    private final static String URL = "vlesdi.hevs.ch:8086/write?db=";
+    private final static String db_name = "";
+    private final static String username = "";
+    private final static String password = "";
+
+
     private static DataBaseConnector instance;
 
+
+    private HttpURLConnection connection;
+    private InputStreamReader inputStream;
+    private OutputStreamWriter outputStream;
     private DataBaseConnector()
     {
+        URL url;
+        try
+        {
+            url = new URL("http://"+username+":"+password+"@"+URL + db_name);
+            connection =(HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "binary/octect-stream");
+            connection.setDoOutput(true);
+
+            inputStream = new InputStreamReader(connection.getInputStream());
+            outputStream = new OutputStreamWriter(connection.getOutputStream());
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
     }
 
@@ -43,7 +74,23 @@ public class DataBaseConnector
 
     private void pushToDB(String label, boolean value)
     {
-        System.out.println("DB:" +label + ":" + value);
+        System.out.println("DB:" + label + ":" + value);
+
+        try
+        {
+            outputStream.write(label + ": value=" + value);
+            char[] buffer = new char[6];
+            inputStream.read(buffer,0,6);
+
+            if(buffer.equals("200 OK"))
+            {
+                System.out.println("Answered correctly");
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
     private void pushToDB(String label, float value)
