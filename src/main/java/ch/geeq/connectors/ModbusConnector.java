0d61b755b7fdd6eb1b27f64d5c8e22ad9a75c541
Weby <weby@we-bb.com>
2017-03-17 10:17:10 +0100

    Removed modbus4j maven dependency.

---
+++ src/main/java/ch/geeq/connectors/ModbusConnector.java
@@ -0,0 +1,10 @@
+package ch.geeq.connectors;
+
+/**
+ * @author weby@we-bb.com [Nicolas Glassey]
+ * @version 1.0.0
+ * @since 3/17/17
+ */
+public class ModbusConnector {
+    private static ModbusConnector instance;
+}
